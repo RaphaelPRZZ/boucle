aeed614bc5280086b21d8fc15c94d9c6488b2dab
RaphaelPRZZ <145431036+RaphaelPRZZ@users.noreply.github.com>
2023-09-22 11:49:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+"use scrict"
+
+/*BOUCLE
+declarer une variable qui est egale a une valeur positif
+faire un console.log de "OKLM" autant de fois que nde valeur a 
+
+*/
+
+let a=4
+for(let i=0; i<a; i++){
+    console.log("OKLM")
+}
